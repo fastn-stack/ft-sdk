32ba31180e9085537ee2a31bfaa8ce47f8df29ab
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-04-18 16:51:58 +0530

    In.now as chrono::DateTime

--- ft-sdk/src/in_.rs
+++ ft-sdk/src/in_.rs
@@ -1,7 +1,7 @@
 pub struct In {
     pub ud: Option<ft_sdk::UserData>,
     pub req: http::Request<bytes::Bytes>,
-    pub now: i64,
+    pub now: chrono::DateTime<chrono::Utc>,
     pub set_cookies: std::collections::HashMap<String, String>,
     pub form_errors: std::collections::HashMap<String, String>,
 }
@@ -11,7 +11,7 @@ impl In {
         Ok(In {
             req,
             // remove the following `.unwrap()` sometime before 2262-04-11T23:47:16.854775807
-            now: ft_sys::now().timestamp_nanos_opt().unwrap(),
+            now: ft_sys::now(),
             ud: ft_sdk::env::ud(),
             set_cookies: std::collections::HashMap::new(),
             form_errors: std::collections::HashMap::new(),
