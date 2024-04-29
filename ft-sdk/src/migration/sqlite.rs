23b386ff4c5f295ed57b03997ee71f9c453be9ca
Amit Upadhyay <upadhyay@gmail.com>
2024-04-29 16:20:05 +0530

    using BLOB instead of Text for data

--- ft-sdk/src/migration/sqlite.rs
+++ ft-sdk/src/migration/sqlite.rs
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS fastn_user
     id       INTEGER PRIMARY KEY,
     name     TEXT NULL,
     username TEXT NULL,
-    data     TEXT -- this stores ft_sdk::auth::UserData
+    data     BLOB -- this stores ft_sdk::auth::UserData
 ) STRICT;
 
 "#;
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS fastn_session
 (
     id   INTEGER PRIMARY KEY,
     uid  INTEGER NULL,
-    data TEXT, -- this is the session data only
+    data BLOB, -- this is the session data only
 
     CONSTRAINT fk_fastn_user
         FOREIGN KEY (uid)
