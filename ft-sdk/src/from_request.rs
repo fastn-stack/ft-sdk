19b9ab840e1be70d8a819e0791fcae33026ac778
Amit Upadhyay <upadhyay@gmail.com>
2024-05-10 14:23:21 +0530

    RequiredString

--- ft-sdk/src/from_request.rs
+++ ft-sdk/src/from_request.rs
@@ -2,29 +2,73 @@ pub trait FromRequest: Sized {
     fn from_request(req: http::Request<bytes::Bytes>) -> Result<Self, ft_sdk::http::Error>;
 }
 
-pub const PATH: usize = 0;
+pub struct Path(pub String);
 
-pub struct Request<const NAME: usize> {
-    v: String,
-}
-
-impl FromRequest for Request<PATH> {
+impl FromRequest for Path {
     fn from_request(req: http::Request<bytes::Bytes>) -> Result<Self, ft_sdk::http::Error> {
-        Ok(Self {
-            v: req.uri().path().to_string(),
-        })
+        let path = req.uri().path().to_string();
+        Ok(Self(path))
     }
 }
 
-pub fn foo(Request { v: path }: Request<PATH>, RString { v: path2 }: RString<"path">) {
-    println!("{}", path2);
-    if path.is_empty() {
-        println!("is empty")
-    } else {
-        println!("{}", path);
+#[derive(Debug)]
+pub struct RequiredString<const KEY: &'static str>(String);
+
+#[derive(Debug)]
+pub struct FieldError {
+    pub field: &'static str,
+    pub error: String,
+}
+
+impl From<FieldError> for ft_sdk::http::Error {
+    fn from(e: FieldError) -> Self {
+        let mut errors = std::collections::HashMap::new();
+        errors.insert(e.field.to_string(), e.error);
+        ft_sdk::http::Error::Form(errors)
     }
 }
 
-pub struct RString<const NAME: &'static str> {
-    v: String,
+impl<const KEY: &'static str> std::fmt::Display for RequiredString<KEY> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0)
+    }
+}
+
+impl<const KEY: &'static str> std::cmp::PartialEq<&str> for RequiredString<KEY> {
+    fn eq(&self, other: &&str) -> bool {
+        self.0 == *other
+    }
+}
+
+impl<const KEY: &'static str> RequiredString<KEY> {
+    pub fn error<S: AsRef<str>>(self, msg: S) -> FieldError {
+        FieldError {
+            field: KEY,
+            error: msg.as_ref().to_string(),
+        }
+    }
+}
+
+impl std::ops::Deref for RequiredString<"username"> {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const KEY: &'static str> FromRequest for RequiredString<KEY> {
+    fn from_request(_req: http::Request<bytes::Bytes>) -> Result<Self, ft_sdk::http::Error> {
+        todo!()
+    }
+}
+
+pub fn foo(Path(path): Path, username: RequiredString<"username">) -> ft_sdk::http::Result {
+    println!("params: {path}, {username}");
+
+    if username == "admin" {
+        Err(username.error("admin is not allowed"))?;
+    }
+
+    todo!()
 }
