5de3778cb22ed4ca32b85f7639e26845187af554
Amit Upadhyay <upadhyay@gmail.com>
2024-05-10 17:05:50 +0530

    more refactor, cleanups etc

---
+++ ft-sdk/src/from_request/required.rs
@@ -0,0 +1,55 @@
+pub struct Required<
+    const KEY: &'static str,
+    T: serde::de::DeserializeOwned + std::fmt::Display + PartialEq = String,
+>(T);
+
+impl<const KEY: &'static str, T: serde::de::DeserializeOwned + std::fmt::Display + PartialEq>
+    std::fmt::Display for Required<KEY, T>
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        T::fmt(&self.0, f)
+    }
+}
+
+impl<const KEY: &'static str, T: serde::de::DeserializeOwned + std::fmt::Display + PartialEq>
+    PartialEq<T> for Required<KEY, T>
+{
+    fn eq(&self, other: &T) -> bool {
+        self.0 == *other
+    }
+}
+
+impl<const KEY: &'static str> PartialEq<&str> for Required<KEY, String> {
+    fn eq(&self, other: &&str) -> bool {
+        self.0 == **other
+    }
+}
+
+impl<const KEY: &'static str, T: serde::de::DeserializeOwned + std::fmt::Display + PartialEq>
+    Required<KEY, T>
+{
+    pub fn error<S: AsRef<str>>(self, msg: S) -> ft_sdk::FieldError {
+        ft_sdk::FieldError {
+            field: KEY,
+            error: msg.as_ref().to_string(),
+        }
+    }
+}
+
+impl<const KEY: &'static str, T: serde::de::DeserializeOwned + std::fmt::Display + PartialEq>
+    std::ops::Deref for Required<KEY, T>
+{
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const KEY: &'static str, T: serde::de::DeserializeOwned + std::fmt::Display + PartialEq>
+    ft_sdk::FromRequest for Required<KEY, T>
+{
+    fn from_request(_req: http::Request<bytes::Bytes>) -> Result<Self, ft_sdk::http::Error> {
+        todo!()
+    }
+}
