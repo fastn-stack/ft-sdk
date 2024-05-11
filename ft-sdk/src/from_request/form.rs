8117f8dbb29b487b0463160b88f789bf9417092a
Amit Upadhyay <upadhyay@gmail.com>
2024-05-11 21:52:15 +0530

    implemented form and required

--- ft-sdk/src/from_request/form.rs
+++ ft-sdk/src/from_request/form.rs
@@ -1,7 +1,7 @@
-pub struct Form<T: serde::de::DeserializeOwned>(T);
+pub struct Form<T: serde::de::DeserializeOwned>(pub T);
 
 impl<T: serde::de::DeserializeOwned> ft_sdk::FromRequest for Form<T> {
-    fn from_request(_req: &http::Request<serde_json::Value>) -> Result<Self, ft_sdk::Error> {
-        todo!()
+    fn from_request(req: &http::Request<serde_json::Value>) -> Result<Self, ft_sdk::Error> {
+        Ok(serde_json::from_value(req.body().clone()).map(Form)?)
     }
 }
