61a091cbe3152920a77fff15c8f8e934a7d1aa80
Amit Upadhyay <upadhyay@gmail.com>
2024-05-11 22:51:07 +0530

    002 compiling

--- examples/002-handling-form/src/lib.rs
+++ examples/002-handling-form/src/lib.rs
@@ -1,24 +1,18 @@
 #[ft_sdk::form]
 fn create_username(
-    site_id: ft_sdk::Hidden<String>,
+    site_id: ft_sdk::Hidden<"site_id">,
     username: ft_sdk::Required<"foo">,
     age: ft_sdk::Optional<"age", i32>,
-) -> ft_sdk::http::ActionResult {
-    if username.is_empty() {
-        return Err(ft_sdk::http::single_error(
-            "username",
-            "username is required",
-        ));
+) -> ft_sdk::form::Result {
+    ft_sdk::println!("{site_id}, {username}, {age}");
+    // let mut errors = vec![];
+    // errors.push(ft_sdk::global_error(
+    //     "you do not have permission to do this",
+    // ));
+    if username == "admin" {
+        return Err(username.error("admin is not allowed").into());
     }
-
-    let mut errors = vec![];
-    errors.push(ft_sdk::global_error(
-        "you do not have permission to do this",
-    ));
-    if username.value() == "admin" {
-        errors.push(username.error("admin account is already take"));
-    }
-    return errors.into();
-
-    ft_sdk::http::redirect("/foo/")
+    // return errors.into();
+    //
+    ft_sdk::form::redirect("/foo/")
 }
--- ft-sdk/src/handler.rs
+++ ft-sdk/src/handler.rs
@@ -53,3 +53,20 @@ where
         (self)(T1::from_request(req)?, T2::from_request(req)?)
     }
 }
+
+impl<F, T1, T2, T3, O> Handler<(T1, T2, T3), O> for F
+where
+    F: Fn(T1, T2, T3) -> Result<O, ft_sdk::Error>,
+    T1: ft_sdk::FromRequest,
+    T2: ft_sdk::FromRequest,
+    T3: ft_sdk::FromRequest,
+{
+    fn call(self, req: &http::Request<serde_json::Value>) -> Result<O, ft_sdk::Error> {
+        // TODO: try to parse both t1 and t2 and return result for both together to clients
+        (self)(
+            T1::from_request(req)?,
+            T2::from_request(req)?,
+            T3::from_request(req)?,
+        )
+    }
+}
