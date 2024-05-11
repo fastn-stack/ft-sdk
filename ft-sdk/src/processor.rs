6cd9d9b2044c8a214d3a311f8ab24d0547e61a93
Amit Upadhyay <upadhyay@gmail.com>
2024-05-11 14:50:43 +0530

    generalising handler over diff outputs

--- ft-sdk/src/processor.rs
+++ ft-sdk/src/processor.rs
@@ -19,50 +19,6 @@ pub fn json<T: serde::Serialize>(t: T) -> Result {
     Ok(Output::Json(serde_json::to_value(t)?))
 }
 
-pub fn handle<T, H: Handler<T>>(h: H) {
-    let req = ft_sdk::http::current_request();
-    let resp = match h.call(&req) {
-        Ok(resp) => resp.into(),
-        Err(e) => {
-            ft_sdk::println!("Error: {:?}", e);
-            e.into()
-        }
-    };
-    // resp.append_cookies(ctx);
-    ft_sdk::http::send_response(resp);
-}
-
-pub trait Handler<T>: Sized {
-    fn call(self, req: &http::Request<bytes::Bytes>) -> ft_sdk::processor::Result;
-}
-
-impl<F> Handler<()> for F
-where
-    F: Fn() -> ft_sdk::processor::Result,
-{
-    fn call(self, _req: &http::Request<bytes::Bytes>) -> ft_sdk::processor::Result {
-        (self)()
-    }
-}
-
-impl<F, T> Handler<T> for F
-where
-    F: Fn(T) -> ft_sdk::processor::Result,
-    T: ft_sdk::FromRequest,
-{
-    fn call(self, req: &http::Request<bytes::Bytes>) -> ft_sdk::processor::Result {
-        (self)(T::from_request(req)?)
-    }
-}
-
-impl<F, T1, T2> Handler<(T1, T2)> for F
-where
-    F: Fn(T1, T2) -> ft_sdk::processor::Result,
-    T1: ft_sdk::FromRequest,
-    T2: ft_sdk::FromRequest,
-{
-    fn call(self, req: &http::Request<bytes::Bytes>) -> ft_sdk::processor::Result {
-        // TODO: try to parse both t1 and t2 and return result for both together to clients
-        (self)(T1::from_request(req)?, T2::from_request(req)?)
-    }
+pub fn redirect<S: AsRef<str>>(url: S) -> Result {
+    Ok(Output::Redirect(url.as_ref().to_string()))
 }
