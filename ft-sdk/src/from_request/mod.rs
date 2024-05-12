045a4f38fd3979320ba9cc4380599d6ebbd575c1
Amit Upadhyay <upadhyay@gmail.com>
2024-05-12 15:17:01 +0530

    some fixes

--- ft-sdk/src/from_request/mod.rs
+++ ft-sdk/src/from_request/mod.rs
@@ -2,9 +2,11 @@
 mod cookie;
 mod field_error;
 mod form;
+#[cfg(feature = "field-extractors")]
 mod hidden;
 mod json;
 mod mountpoint;
+#[cfg(feature = "field-extractors")]
 mod optional;
 mod path;
 #[cfg(feature = "field-extractors")]
@@ -24,16 +26,4 @@ pub trait FromRequest: Sized {
 
 pub trait FromJsonBody: Sized {
     fn from_json_body(body: &serde_json::Value) -> Result<Self, ft_sdk::Error>;
-}
-
-#[allow(dead_code)]
-pub fn foo(path: ft_sdk::Path, username: ft_sdk::Required<"username">) -> ft_sdk::http::Result {
-    println!("params: {path}, {username}");
-
-    if username == "admin" {
-        Err(username.error("admin is not allowed"))?;
-    }
-
-    // ft_sdk::json().with_cookie(k, v).with_header(k2, v2)
-    todo!()
 }
