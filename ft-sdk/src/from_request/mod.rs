a109f6653adaa7afa21c40b0c956dedc50364990
Amit Upadhyay <upadhyay@gmail.com>
2024-05-12 16:48:57 +0530

    one rejected solution

--- ft-sdk/src/from_request/mod.rs
+++ ft-sdk/src/from_request/mod.rs
@@ -22,8 +22,28 @@ pub use {cookie::Cookie, hidden::Hidden, optional::Optional, query::Query, requi
 
 pub trait FromRequest: Sized {
     fn from_request(req: &http::Request<serde_json::Value>) -> Result<Self, ft_sdk::Error>;
+    fn post_process_form_output(
+        &self,
+        output: ft_sdk::form::Output,
+    ) -> Result<ft_sdk::form::Output, ft_sdk::Error> {
+        Ok(output)
+    }
+    fn post_process_processor_output(
+        &self,
+        output: ft_sdk::processor::Output,
+    ) -> Result<ft_sdk::processor::Output, ft_sdk::Error> {
+        Ok(output)
+    }
+    fn post_process_data_output(
+        &self,
+        output: ft_sdk::data::Output,
+    ) -> Result<ft_sdk::data::Output, ft_sdk::Error> {
+        Ok(output)
+    }
 }
 
-pub trait FromJsonBody: Sized {
-    fn from_json_body(body: &serde_json::Value) -> Result<Self, ft_sdk::Error>;
+impl FromRequest for chrono::DateTime<chrono::Utc> {
+    fn from_request(_req: &http::Request<serde_json::Value>) -> Result<Self, ft_sdk::Error> {
+        Ok(ft_sdk::env::now())
+    }
 }
