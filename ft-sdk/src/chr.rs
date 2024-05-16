8e8e16712b412bf6656a918bb8ec340c5229ddda
Amit Upadhyay <upadhyay@gmail.com>
2024-05-16 13:22:33 +0530

    revived some tests, fixed a bug caught by test

--- ft-sdk/src/chr.rs
+++ ft-sdk/src/chr.rs
@@ -26,176 +26,152 @@ pub(crate) fn chr(
             .try_append(http::header::SET_COOKIE, cookie)?;
     }
     for (k, v) in headers.into_iter() {
-        response.headers_mut().try_append(k, v)?;
+        response.headers_mut().insert(k, v);
     }
 
     Ok(response)
 }
 
-// pub trait IntoCookie {
-//     fn into_cookie(self) -> http::HeaderValue;
-// }
-//
-// impl IntoCookie for http::HeaderValue {
-//     fn into_cookie(self) -> http::HeaderValue {
-//         self
-//     }
-// }
-//
-// impl<K: AsRef<str>, V: AsRef<str>> IntoCookie for (K, V) {
-//     fn into_cookie(self) -> http::HeaderValue {
-//         let (k, v) = self;
-//         format!(
-//             "{}={}; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000",
-//             k.as_ref(),
-//             v.as_ref()
-//         )
-//         .parse()
-//         .unwrap()
-//     }
-// }
-//
-// impl<K: AsRef<str>, V: AsRef<str>> IntoCookie for (K, V, i32) {
-//     fn into_cookie(self) -> http::HeaderValue {
-//         let (k, v, max_age) = self;
-//         format!(
-//             "{}={}; Secure; HttpOnly; SameSite=Strict; Max-Age={max_age}",
-//             k.as_ref(),
-//             v.as_ref()
-//         )
-//         .parse()
-//         .unwrap()
-//     }
-// }
-//
-// impl Output {
-//     pub fn with_cookie<C: IntoCookie>(self, c: C) -> Self {
-//         let mut r: http::Response<bytes::Bytes> = self.into();
-//         let cookie: http::HeaderValue = c.into_cookie();
-//
-//         match r.headers_mut().entry(http::header::SET_COOKIE) {
-//             http::header::Entry::Vacant(entry) => {
-//                 entry.insert(cookie);
-//             }
-//             http::header::Entry::Occupied(mut entry) => {
-//                 entry.append(cookie);
-//             }
-//         };
-//
-//         Output::Http(r)
-//     }
-//
-//     pub fn with_header<K: http::header::IntoHeaderName>(
-//         self,
-//         key: K,
-//         value: http::HeaderValue,
-//     ) -> Self {
-//         let mut r: http::Response<bytes::Bytes> = self.into();
-//         r.headers_mut().insert(key, value);
-//         Output::Http(r)
-//     }
-// }
+pub trait IntoCookie {
+    fn into_cookie(self) -> http::HeaderValue;
+}
 
-// #[cfg(test)]
-// mod test {
-//     #[test]
-//     fn header() {
-//         let r: http::Response<bytes::Bytes> = super::Output::Reload
-//             .with_header(
-//                 http::header::CONTENT_TYPE,
-//                 http::HeaderValue::from_static("text/html"),
-//             )
-//             .into();
-//
-//         assert_eq!(
-//             r.headers().get(http::header::CONTENT_TYPE),
-//             Some(&http::HeaderValue::from_static("text/html"))
-//         );
-//
-//         let r: http::Response<bytes::Bytes> = super::Output::Reload
-//             .with_header("content-type", http::HeaderValue::from_static("text/html"))
-//             .into();
-//
-//         assert_eq!(
-//             r.headers().get(http::header::CONTENT_TYPE),
-//             Some(&http::HeaderValue::from_static("text/html"))
-//         );
-//     }
-//
-//     #[test]
-//     fn cookie() {
-//         let r: http::Response<bytes::Bytes> =
-//             super::Output::Reload.with_cookie(("name", "value")).into();
-//
-//         let cookies = r.headers().get_all(http::header::SET_COOKIE);
-//         let mut iter = cookies.iter();
-//         assert_eq!(
-//             iter.next(),
-//             Some(&http::HeaderValue::from_static(
-//                 "name=value; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000"
-//             ))
-//         );
-//         assert_eq!(iter.next(), None);
-//
-//         let r: http::Response<bytes::Bytes> = super::Output::Reload
-//             .with_cookie(("name", "value", 200))
-//             .into();
-//
-//         let cookies = r.headers().get_all(http::header::SET_COOKIE);
-//         let mut iter = cookies.iter();
-//         assert_eq!(
-//             iter.next(),
-//             Some(&http::HeaderValue::from_static(
-//                 "name=value; Secure; HttpOnly; SameSite=Strict; Max-Age=200"
-//             ))
-//         );
-//         assert_eq!(iter.next(), None);
-//
-//         let r: http::Response<bytes::Bytes> = super::Output::Reload
-//             .with_cookie(("name", "value"))
-//             .with_cookie(("n2", "v2"))
-//             .into();
-//
-//         let cookies = r.headers().get_all(http::header::SET_COOKIE);
-//         let mut iter = cookies.iter();
-//         assert_eq!(
-//             iter.next(),
-//             Some(&http::HeaderValue::from_static(
-//                 "name=value; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000"
-//             ))
-//         );
-//         assert_eq!(
-//             iter.next(),
-//             Some(&http::HeaderValue::from_static(
-//                 "n2=v2; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000"
-//             ))
-//         );
-//     }
-//
-//     #[test]
-//     fn raw_cookie() {
-//         let r: http::Response<bytes::Bytes> = super::Output::Reload
-//             .with_cookie(http::HeaderValue::from_static("hello"))
-//             .into();
-//
-//         let cookies = r.headers().get_all(http::header::SET_COOKIE);
-//         let mut iter = cookies.iter();
-//         assert_eq!(iter.next(), Some(&http::HeaderValue::from_static("hello")));
-//         assert_eq!(iter.next(), None);
-//
-//         let r: http::Response<bytes::Bytes> = super::Output::Reload
-//             .with_cookie(("name", "value"))
-//             .with_cookie(http::HeaderValue::from_static("hello"))
-//             .into();
-//
-//         let cookies = r.headers().get_all(http::header::SET_COOKIE);
-//         let mut iter = cookies.iter();
-//         assert_eq!(
-//             iter.next(),
-//             Some(&http::HeaderValue::from_static(
-//                 "name=value; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000"
-//             ))
-//         );
-//         assert_eq!(iter.next(), Some(&http::HeaderValue::from_static("hello")));
-//     }
-// }
+impl IntoCookie for http::HeaderValue {
+    fn into_cookie(self) -> http::HeaderValue {
+        self
+    }
+}
+
+impl<K: AsRef<str>, V: AsRef<str>> IntoCookie for (K, V) {
+    fn into_cookie(self) -> http::HeaderValue {
+        let (k, v) = self;
+        format!(
+            "{}={}; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000",
+            k.as_ref(),
+            v.as_ref()
+        )
+        .parse()
+        .unwrap()
+    }
+}
+
+impl<K: AsRef<str>, V: AsRef<str>> IntoCookie for (K, V, i32) {
+    fn into_cookie(self) -> http::HeaderValue {
+        let (k, v, max_age) = self;
+        format!(
+            "{}={}; Secure; HttpOnly; SameSite=Strict; Max-Age={max_age}",
+            k.as_ref(),
+            v.as_ref()
+        )
+        .parse()
+        .unwrap()
+    }
+}
+
+impl<O> CHR<O> {
+    pub fn with_cookie<C: IntoCookie>(mut self, c: C) -> Self {
+        self.cookies.push(c.into_cookie());
+        self
+    }
+
+    pub fn with_header(mut self, key: http::HeaderName, value: http::HeaderValue) -> Self {
+        self.headers.push((key, value));
+        self
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn header() {
+        let r = ft_sdk::json(()).unwrap();
+        let chr = super::CHR::new(()).with_header(
+            http::header::CONTENT_TYPE,
+            http::HeaderValue::from_static("text/html"),
+        );
+        let r = super::chr(chr.cookies, chr.headers, r).unwrap();
+
+        assert_eq!(
+            r.headers().get(http::header::CONTENT_TYPE),
+            Some(&http::HeaderValue::from_static("text/html"))
+        );
+    }
+
+    #[test]
+    fn cookie() {
+        let r = ft_sdk::json(()).unwrap();
+        let chr = super::CHR::new(()).with_cookie(("name", "value"));
+        let r = super::chr(chr.cookies, chr.headers, r).unwrap();
+
+        let cookies = r.headers().get_all(http::header::SET_COOKIE);
+        let mut iter = cookies.iter();
+        assert_eq!(
+            iter.next(),
+            Some(&http::HeaderValue::from_static(
+                "name=value; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000"
+            ))
+        );
+        assert_eq!(iter.next(), None);
+
+        //     let r: http::Response<bytes::Bytes> = super::Output::Reload
+        //         .with_cookie(("name", "value", 200))
+        //         .into();
+        //
+        //     let cookies = r.headers().get_all(http::header::SET_COOKIE);
+        //     let mut iter = cookies.iter();
+        //     assert_eq!(
+        //         iter.next(),
+        //         Some(&http::HeaderValue::from_static(
+        //             "name=value; Secure; HttpOnly; SameSite=Strict; Max-Age=200"
+        //         ))
+        //     );
+        //     assert_eq!(iter.next(), None);
+        //
+        //     let r: http::Response<bytes::Bytes> = super::Output::Reload
+        //         .with_cookie(("name", "value"))
+        //         .with_cookie(("n2", "v2"))
+        //         .into();
+        //
+        //     let cookies = r.headers().get_all(http::header::SET_COOKIE);
+        //     let mut iter = cookies.iter();
+        //     assert_eq!(
+        //         iter.next(),
+        //         Some(&http::HeaderValue::from_static(
+        //             "name=value; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000"
+        //         ))
+        //     );
+        //     assert_eq!(
+        //         iter.next(),
+        //         Some(&http::HeaderValue::from_static(
+        //             "n2=v2; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000"
+        //         ))
+        //     );
+    }
+    //
+    // #[test]
+    // fn raw_cookie() {
+    //     let r: http::Response<bytes::Bytes> = super::Output::Reload
+    //         .with_cookie(http::HeaderValue::from_static("hello"))
+    //         .into();
+    //
+    //     let cookies = r.headers().get_all(http::header::SET_COOKIE);
+    //     let mut iter = cookies.iter();
+    //     assert_eq!(iter.next(), Some(&http::HeaderValue::from_static("hello")));
+    //     assert_eq!(iter.next(), None);
+    //
+    //     let r: http::Response<bytes::Bytes> = super::Output::Reload
+    //         .with_cookie(("name", "value"))
+    //         .with_cookie(http::HeaderValue::from_static("hello"))
+    //         .into();
+    //
+    //     let cookies = r.headers().get_all(http::header::SET_COOKIE);
+    //     let mut iter = cookies.iter();
+    //     assert_eq!(
+    //         iter.next(),
+    //         Some(&http::HeaderValue::from_static(
+    //             "name=value; Secure; HttpOnly; SameSite=Strict; Max-Age=34560000"
+    //         ))
+    //     );
+    //     assert_eq!(iter.next(), Some(&http::HeaderValue::from_static("hello")));
+    // }
+}
