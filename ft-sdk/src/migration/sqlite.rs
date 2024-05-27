8228e3ce2fc6b787b9971595795cb6989587a44f
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-05-27 18:13:24 +0530

    Added updated_at column in fastn_email_queue

--- ft-sdk/src/migration/sqlite.rs
+++ ft-sdk/src/migration/sqlite.rs
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS fastn_email_queue
     body_html    TEXT NOT NULL,
     retry_count  INTEGER NOT NULL DEFAULT 0,
     created_at   INTEGER NOT NULL,
+    updated_at   INTEGER NOT NULL,
     sent_at      INTEGER NOT NULL,
     -- mkind is any string, used for product analytics etc
     mkind        TEXT NOT NULL,
