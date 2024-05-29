c37a3b5d6f00774eabc9f83c581deaf3cc5b1996
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-05-29 12:56:07 +0530

    Rename zip to more general type binary

--- ft-sdk/src/data.rs
+++ ft-sdk/src/data.rs
@@ -1,9 +1,9 @@
 pub type Result = std::result::Result<ft_sdk::chr::CHR<Output>, ft_sdk::Error>;
 
 #[derive(Debug)]
-pub enum Output {
+pub(crate) enum Output {
     Json(serde_json::Value),
-    Zip((String, bytes::Bytes)),
+    Binary(ft_sdk::Binary),
 }
 
 impl From<ft_sdk::chr::CHR<Output>>
@@ -17,18 +17,53 @@ impl From<ft_sdk::chr::CHR<Output>>
         }: ft_sdk::chr::CHR<Output>,
     ) -> Self {
         let response = match response {
-            Output::Json(j) => crate::json(j),
-            Output::Zip((filename, content)) => crate::zip(filename.as_str(), content),
+            Output::Json(json_value) => ft_sdk::json(json_value),
+            Output::Binary(binary) => ft_sdk::binary(binary),
         }?;
         ft_sdk::chr::chr(cookies, headers, response)
     }
 }
 
-pub fn zip(filename: &str, content: bytes::Bytes) -> Result {
-    Ok(ft_sdk::chr::CHR::new(Output::Zip((
-        filename.to_string(),
-        content,
-    ))))
+
+/// Creates a binary response for serving binary data over HTTP.
+///
+/// # Parameters
+/// - `filename`: An optional `String` representing the name of the file. If provided, the response
+///   will add `Content-Disposition: attachment; filename="{filename}"` as header, indicating the
+///   binary should be downloaded and this name will be used as the filename for the download.
+/// - `content`: A `bytes::Bytes` object containing the binary content.
+/// - `content_type`: A `String` specifying the MIME type of the content (e.g., `application/pdf`,
+///   `image/png`).
+///
+/// # Example
+/// ```rust
+/// use bytes::Bytes;
+/// use ft_sdk::binary;
+///
+/// let filename = Some(String::from("example.txt"));
+/// let content = Bytes::from("This is the content of the file.");
+/// let content_type = String::from("text/plain");
+///
+/// match binary(filename, content, content_type) {
+///     Ok(response) => {
+///         // Process the response, such as sending it over HTTP
+///         println!("Binary response created successfully!");
+///     },
+///     Err(e) => {
+///         eprintln!("Failed to create binary response: {:?}", e);
+///     }
+/// }
+/// ```
+pub fn binary(
+    filename: Option<String>,
+    content: bytes::Bytes,
+    content_type: String
+) -> Result {
+    Ok(ft_sdk::chr::CHR::new(Output::Binary(ft_sdk::Binary {
+        file_name,
+        data,
+        content_type,
+    })))
 }
 
 pub fn json<T: serde::Serialize>(t: T) -> Result {
