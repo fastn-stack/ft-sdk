ceb6021de7255940243306d49cc7835354b3060c
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-05-31 16:06:36 +0530

    Migration: 0003-fastn-user-identity-nullable-and-fastn-session-data-non-nullable

--- ft-sdk/migrations/0003-fastn-user-identity-nullable-and-fastn-session-data-non-nullable.sql
+++ ft-sdk/migrations/0003-fastn-user-identity-nullable-and-fastn-session-data-non-nullable.sql
@@ -1,7 +1,7 @@
--- Step 1: Disable foreign key constraint enforcement (if necessary)
+-- Disable foreign key constraint enforcement (if necessary)
 PRAGMA defer_foreign_keys = ON;
 
--- Step 2: Create a new table with the desired schema
+-- Create a new table with the desired schema
 CREATE TABLE IF NOT EXISTS "new__fastn_user"
 (
     id       INTEGER PRIMARY KEY,
@@ -12,15 +12,15 @@ CREATE TABLE IF NOT EXISTS "new__fastn_user"
     updated_at INTEGER NOT NULL
 ) STRICT;
 
--- Step 3: Copy data from the old table to the new table
+-- Copy data from the old table to the new table
 INSERT INTO "new__fastn_user" (id, name, identity, data, created_at, updated_at)
 SELECT id, name, identity, data, created_at, updated_at
 FROM fastn_user;
 
--- Step 4: Drop the old table
+-- Drop the old table
 DROP TABLE fastn_user;
 
--- Step 5: Rename the new table to the old table's name
+-- Rename the new table to the old table's name
 ALTER TABLE "new__fastn_user" RENAME TO fastn_user;
 
 
@@ -30,21 +30,21 @@ ALTER TABLE "new__fastn_user" RENAME TO fastn_user;
 
 
 
--- Make fastn_session::data NOT NULL
-UPDATE fastn_session SET data='{}' WHERE data = null or data = '';
+-- Update NULL or empty `data` entries in the old table
+UPDATE fastn_session SET data = '{}' WHERE data IS NULL OR data = '';
 
 CREATE TABLE IF NOT EXISTS "new__fastn_session"
 (
     id   TEXT PRIMARY KEY,
     uid  INTEGER NULL,
-    data TEXT NOT NULL, -- this is the session data only
+    data TEXT NOT NULL, -- Make `data` column NOT NULL
     created_at INTEGER NOT NULL,
     updated_at INTEGER NOT NULL,
 
     CONSTRAINT fk_fastn_user
     FOREIGN KEY (uid)
     REFERENCES fastn_user (id)
-    ) STRICT;
+) STRICT;
 
 
 INSERT INTO "new__fastn_session" (id, uid, data, created_at, updated_at)
@@ -58,5 +58,5 @@ ALTER TABLE "new__fastn_session" RENAME TO fastn_session;
 
 
 
--- Step 6: Re-enable foreign key constraint enforcement (if necessary)
+-- Re-enable foreign key constraint enforcement (if necessary)
 PRAGMA defer_foreign_keys = OFF;
