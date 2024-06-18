fbecd9348e15ab87f7f83058f4fc95856646f85b
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-06-19 00:45:12 +0530

    Minor fix

--- ft-sdk/src/auth/mod.rs
+++ ft-sdk/src/auth/mod.rs
@@ -6,7 +6,7 @@ mod utils;
 
 pub use ft_sys_shared::SESSION_KEY;
 pub use schema::{fastn_session, fastn_user};
-pub use session::SessionID;
+pub use session::{SessionID, set_session_cookie, expire_session_cookie};
 pub use utils::{user_data_by_query, Counter};
 
 #[derive(Clone, Debug)]
