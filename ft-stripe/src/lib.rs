ae02d75889404b4a0519a8791f9bf71be87b5786
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-06-20 20:29:50 +0530

    added customer apis

--- ft-stripe/src/lib.rs
+++ ft-stripe/src/lib.rs
@@ -2,15 +2,18 @@ extern crate self as ft_stripe;
 
 mod client;
 mod error;
+mod customer;
+mod params;
 
 mod config {
-    pub type Client = crate::client::Client;
-    pub type Response<T> = Result<T, crate::error::StripeError>;
+    pub type Client = ft_stripe::client::Client;
+    pub type Response<T> = Result<T, ft_stripe::error::StripeError>;
 }
 
 pub use self::config::Client;
 pub use self::config::Response;
 
-pub use stripe::*;
+pub use stripe::{CreateCustomer, Customer, CustomerId, List, ListCustomers, UpdateCustomer};
+use stripe::*;
 
 
