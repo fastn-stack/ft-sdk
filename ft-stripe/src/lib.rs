9fadca3c52bf8aee7a478bc043f59476c5dd51dc
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-06-20 23:15:35 +0530

    Fix imports

--- ft-stripe/src/lib.rs
+++ ft-stripe/src/lib.rs
@@ -1,14 +1,13 @@
 extern crate self as ft_stripe;
 
 mod client;
+pub use crate::client::*;
+
 mod error;
-mod customer;
+pub use crate::error::{ErrorCode, ErrorType, RequestError, StripeError, WebhookError};
+
 mod async_stripe;
 pub(crate) use async_stripe::{ids, resources, params};
-
-
-pub use crate::client::*;
-pub use crate::error::{ErrorCode, ErrorType, RequestError, StripeError, WebhookError};
 pub use crate::async_stripe::ids::*;
 pub use crate::async_stripe::params::{
     Expandable, Headers, IdOrCreate, List, Metadata, Object, RangeBounds, RangeQuery, SearchList,
