43808de3c158a793ce8c379c95741c7a4368574c
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-06-20 23:29:06 +0530

    Move error inside async_stripe

--- ft-stripe/src/lib.rs
+++ ft-stripe/src/lib.rs
@@ -3,14 +3,12 @@ extern crate self as ft_stripe;
 mod client;
 pub use crate::client::*;
 
-mod error;
-pub use crate::error::{ErrorCode, ErrorType, RequestError, StripeError, WebhookError};
-
 mod async_stripe;
-pub(crate) use async_stripe::{ids, resources, params};
+pub(crate) use async_stripe::{ids, resources, params, error};
 pub use crate::async_stripe::ids::*;
 pub use crate::async_stripe::params::{
     Expandable, Headers, IdOrCreate, List, Metadata, Object, RangeBounds, RangeQuery, SearchList,
     Timestamp,
 };
 pub use crate::async_stripe::resources::*;
+pub use crate::async_stripe::error::{ErrorCode, ErrorType, RequestError, StripeError, WebhookError};
