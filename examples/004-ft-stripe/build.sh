8e88d3ebb1dd251b651c7add1b066e8dd4e3f3b9
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-06-24 12:33:09 +0530

    Rename 004 example to stripe-example

--- examples/004-ft-stripe/build.sh
+++ examples/004-ft-stripe/build.sh
@@ -3,4 +3,4 @@
 
 cargo build --target wasm32-unknown-unknown --release
 # make sure to change the name of the wasm file to match the name in Cargo.toml
-cp ../../target/wasm32-unknown-unknown/release/hello_world.wasm .
+cp ../../target/wasm32-unknown-unknown/release/stripe_example.wasm .
