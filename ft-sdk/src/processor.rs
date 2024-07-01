4610e4bcba07f7e4bc1ada134d45a9ecdd3aed2b
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-07-01 14:57:53 +0530

    Added BrowserRedirection in processor

--- ft-sdk/src/processor.rs
+++ ft-sdk/src/processor.rs
@@ -2,8 +2,12 @@ pub type Result = std::result::Result<ft_sdk::chr::CHR<Output>, ft_sdk::Error>;
 
 #[derive(Debug)]
 pub enum Output {
+    /// This variant is used for redirection.
     Redirect(u16, String),
+    /// This variant is used for returning JSON data.
     Json(serde_json::Value),
+    /// This variant is intended for setting cookies and then redirecting the browser.
+    BrowserRedirect(String),
 }
 
 impl Output {
@@ -14,6 +18,7 @@ impl Output {
         match self {
             Output::Redirect(code, url) => Output::Redirect(code, url),
             Output::Json(j) => Output::Json(f(j)),
+            Output::BrowserRedirect(url) => Output::BrowserRedirect(url),
         }
     }
 }
@@ -34,6 +39,14 @@ impl From<ft_sdk::chr::CHR<Output>>
                 .header("Location", url)
                 .body("".into())?),
             Output::Json(j) => ft_sdk::json(j),
+            Output::BrowserRedirect(url) => Ok(http::Response::builder()
+                // This status code indicates that the request has been accepted for processing,
+                // but the processing is not yet complete. It's a way to acknowledge that the
+                // request has been received and that further action (the redirection) will be
+                // taken by the client.
+                .status(202)
+                .header("content-type", "text/html; charset=utf-8")
+                .body(format!("<meta http-equiv='refresh' content='0; url={url}' />").into())?)
         }?;
         ft_sdk::chr::chr(cookies, headers, response)
     }
@@ -45,6 +58,11 @@ pub fn json<T: serde::Serialize>(j: T) -> Result {
     )?)))
 }
 
+pub fn browser_redirect<S: AsRef<str>>(url: S) -> Result {
+    Ok(ft_sdk::chr::CHR::new(Output::BrowserRedirect(
+        url.as_ref().to_string(),
+    )))
+}
 pub fn permanent_redirect<S: AsRef<str>>(url: S) -> Result {
     Ok(ft_sdk::chr::CHR::new(Output::Redirect(
         308,
